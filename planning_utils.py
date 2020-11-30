1cf29cb57cb0bf395089ed8190e9d6087a813ed4
Joseph Gatto <35414926+JosephGatto@users.noreply.github.com>
2020-11-29 22:14:16 -0500

    Add files via upload

---
+++ planning_utils.py
@@ -0,0 +1,162 @@
+import sys
+import shapely
+from shapely.geometry import box, MultiPolygon, LineString, Point, MultiLineString, Polygon
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+# %matplotlib inline
+
+from gridmap import OccupancyGridMap
+import matplotlib.pyplot as plt
+from a_star import a_star
+from utils import plot_path
+from sklearn import preprocessing
+import pandas as pd
+from tqdm import tqdm
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import f1_score, make_scorer
+from sklearn.model_selection import cross_val_score, train_test_split
+import random
+from tqdm import tqdm
+import imageio
+
+import warnings
+warnings.filterwarnings('ignore')
+
+def gen_shapely_map(map_size, obstacles):
+    '''
+    Given map_size and list of obstacles where
+    an obstacle is described by a tuple (xmin, ymin, xmax, ymax),
+    create a shapely map. 
+    '''
+    
+    pols = [box(0,0,map_size[0], map_size[1])]
+    for obstacle in obstacles:
+        pols.append(box(*obstacle))
+    lines = []
+    for pol in pols:
+        boundary = pol.boundary
+        if boundary.type == 'MultiLineString':
+            for line in boundary:
+                lines.append(line)
+        else:
+            lines.append(boundary)
+
+    MAP = MultiLineString(lines)
+    return MAP
+
+
+def get_map_arr(MAP_params, shape):
+    '''
+    Convert the shapely obstacles into a numpy grid
+    for use with the A* planner. 
+    
+    MAP_params is of the form [(xmin, ymin, xmax, ymax), (xmin, ymin, xmax, ymax), ...]
+    describing each rectangular obstacle in the map. Currently only supports rectangles. 
+    
+    Shape is just a tuple with the map size. 
+    
+    '''
+    map_np = np.zeros(shape)
+    for param in MAP_params:
+        xmin, ymin, xmax, ymax = param
+        map_np[ymin:ymax, xmin:xmax] = 1
+        
+    return map_np
+
+def get_path(start, goal, arr, plot = True):
+    '''
+    Given start (x,y) and goal (x,y) use numpy grid arr
+    to solve for best path with A*. 
+    
+    By default, plot the A* path everytime this is called. 
+    '''
+    resolution = 1
+    gmap = OccupancyGridMap(arr, resolution)
+
+    # 4n = 4 point connectivity. 8N = 8 point connectivity
+    path, path_px = a_star(start, goal, gmap, movement='4N')
+    if plot:
+        gmap.plot()
+        plot_path(path_px)
+    return path
+
+
+def make_tuple(x):
+    '''
+    Function used to turn 2 pandas columns into
+    a tuple in one column. See create_classification_problem
+    for use. 
+    '''
+    return (x.iloc[0], x.iloc[1])
+
+def get_inv_dirs(x):
+    '''
+    Function to encode direction tuple according to hard-coded encoding
+    '''
+    return inv_dirs[x]
+
+def get_steer(x):
+    '''
+    Get either ccw, cw or straight
+    '''
+    if x == 1 or x == -3: # ccw
+        return 1
+    elif x == -1 or x == 3: # cw
+        return 2
+    else: return 0 # straight
+
+def create_classification_problem(df, num_sensor_readings, one_hot=False, robot_type = 'omni'):
+    '''
+    Now that training data has been synthesized, 
+    prepare data for use with ML model. 
+    '''
+    
+    if robot_type == 'omni':
+        df['out'] = df[[num_sensor_readings+2, num_sensor_readings+3]].apply(make_tuple, axis=1)
+        df['out'] = df['out'].astype(str)
+        
+        # Drop the sample where we are at the target location. 
+        # We don't want to learn to stay still. 
+        df = df[df['out']!='(0.0, 0.0)']
+     
+        # Label encode targets
+        enc = preprocessing.LabelEncoder()
+        df['out'] = enc.fit_transform(df['out'])
+        df.drop([num_sensor_readings+2, num_sensor_readings+3], axis=1, inplace=True)
+    else:
+
+
+        df['out'] = df[num_sensor_readings+2]
+        df = df[df['out'] != 2] # drop 180 degrees turn which can only happen at start node
+        df = df[df['out'] != -2]
+        df['out'] = df['out'].apply(get_steer)
+        
+        df.drop([num_sensor_readings+2], axis=1, inplace=True)
+
+    df = df.sample(frac=1)
+    
+    return df
+
+def plot_path_(MAP, robot_location, goal_location, path):
+    '''
+    Plot path on map using shapely. Not currently used. 
+    '''
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(6,6)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    fig.add_axes(ax)
+    
+    for index, l in enumerate(MAP): 
+        if index != 0:
+            ax.fill(*l.xy, alpha=1)
+        else:
+            ax.plot(*l.xy, alpha=1)
+    
+    ax.plot(*LineString(path).xy)
+    ax.scatter(robot_location[0], robot_location[1], s=30, alpha=1.0) 
+    ax.scatter(goal_location[0], goal_location[1], s=30, alpha=1.0) 
